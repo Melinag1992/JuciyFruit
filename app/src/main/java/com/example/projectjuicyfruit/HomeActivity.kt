f557e90c763daada84fe12524842d21395689ded
joseog-geniusplaza <jose@geniusplaza.com>
2021-01-17 02:15:24 -0500

    update .gitignore

--- app/src/main/java/com/example/projectjuicyfruit/HomeActivity.kt
+++ app/src/main/java/com/example/projectjuicyfruit/HomeActivity.kt
@@ -2,7 +2,7 @@ package com.example.projectjuicyfruit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.projectjuicyfruit.screens.DashboardFragment
+import com.example.projectjuicyfruit.views.DashboardFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
